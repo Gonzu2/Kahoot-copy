24286a83cdd2b90f1c26897091956ae91ee9d6c1
Gonzu2 <gonzuwu2@gmail.com>
2023-04-03 20:25:52 +0300

    Fixed wrong text at how kahoot works section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -444,10 +444,10 @@ function Home({ quizes }) {
                   <div class="skew-inner"></div>
                 </div>
               </div>
-              <h1 class="card-header">Create</h1>
+              <h1 class="card-header">Host or share</h1>
               <p class="card-info">
-                It only takes minutes to create a learning game or trivia quiz
-                on any topic, in any language.
+                Host a live game with questions on a big screen or share a game
+                with remote players.
               </p>
             </li>
             <li class="card">
@@ -470,43 +470,68 @@ function Home({ quizes }) {
                   <div class="skew-inner"></div>
                 </div>
               </div>
-              <h1 class="card-header">Create</h1>
+              <h1 class="card-header">Play</h1>
               <p class="card-info">
-                It only takes minutes to create a learning game or trivia quiz
-                on any topic, in any language.
+                Game on! Join a kahoot with a PIN provided by the host and
+                answer questions on your device.
               </p>
             </li>
           </ul>
           <div class="play-ad">
             <div className="play-ad-wrapper">
-            <div class="play-ad-inner">
-            <h1 class="play-ad-info">
-                Play Kahoot! to see how it works!
-                <a href="#" class="play-ad-button underline">
-                  Explore our featured games!
-                </a>
-              </h1>
-              <div className="hands" id="left">
-              <svg width="40px" height="39px" viewBox="0 0 40 39" class="hand" aria-hidden="true">
-                            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                <path class="hand__grip" d="M6,32 L7.1340332,11 L29.0739746,11 L30.9841309,6.30419922 C32.5002016,-0.497833236 39.8556292,0.124038746 39.8556292,0.124038746 L36.2359774,20.1807405 L19.7161246,22.3503189 L18.9752808,31.1017456 L6,32 Z" fill="#FAD09E"></path>
-                                <polygon class="hand__cuff" points="0.000131427821 39 0.000131427821 31.9825647 19.6968384 31 23 39"></polygon>
-                            </g>
-                        </svg>
-              </div>
-              <div className="hands" id="right">
-
-              <svg width="40px" height="39px" viewBox="0 0 40 39" class="hand" aria-hidden="true">
-                            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                            <path class="hand__grip" d="M20.9,31.1l-0.7-8.8L3.6,20.2L0,0.1c0,0,7.4-0.6,8.9,6.2l1.9,4.7h21.9l1.1,21L20.9,31.1z" fill="#FAD09E"></path>
-                            <polygon class="hand__cuff" points="16.9,39 20.2,31 39.9,32 39.9,39"></polygon>
-                        </g>
-                        </svg>
+              <div class="play-ad-inner">
+                <h1 class="play-ad-info">
+                  Play Kahoot! to see how it works!
+                  <a href="#" class="play-ad-button underline">
+                    Explore our featured games!
+                  </a>
+                </h1>
+                <div className="hands" id="left">
+                  <svg
+                    width="40px"
+                    height="39px"
+                    viewBox="0 0 40 39"
+                    class="hand"
+                    aria-hidden="true">
+                    <g
+                      stroke="none"
+                      stroke-width="1"
+                      fill="none"
+                      fill-rule="evenodd">
+                      <path
+                        class="hand__grip"
+                        d="M6,32 L7.1340332,11 L29.0739746,11 L30.9841309,6.30419922 C32.5002016,-0.497833236 39.8556292,0.124038746 39.8556292,0.124038746 L36.2359774,20.1807405 L19.7161246,22.3503189 L18.9752808,31.1017456 L6,32 Z"
+                        fill="#FAD09E"></path>
+                      <polygon
+                        class="hand__cuff"
+                        points="0.000131427821 39 0.000131427821 31.9825647 19.6968384 31 23 39"></polygon>
+                    </g>
+                  </svg>
+                </div>
+                <div className="hands" id="right">
+                  <svg
+                    width="40px"
+                    height="39px"
+                    viewBox="0 0 40 39"
+                    class="hand"
+                    aria-hidden="true">
+                    <g
+                      stroke="none"
+                      stroke-width="1"
+                      fill="none"
+                      fill-rule="evenodd">
+                      <path
+                        class="hand__grip"
+                        d="M20.9,31.1l-0.7-8.8L3.6,20.2L0,0.1c0,0,7.4-0.6,8.9,6.2l1.9,4.7h21.9l1.1,21L20.9,31.1z"
+                        fill="#FAD09E"></path>
+                      <polygon
+                        class="hand__cuff"
+                        points="16.9,39 20.2,31 39.9,32 39.9,39"></polygon>
+                    </g>
+                  </svg>
+                </div>
               </div>
             </div>
-            </div>
-
-            
           </div>
         </div>
         {/* How does kahoot work end */}
