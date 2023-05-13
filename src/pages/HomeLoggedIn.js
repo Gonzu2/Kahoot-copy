2b843b78855dd58f89710bbc6363f21a5f33b805
Gonzu2 <gonzuwu2@gmail.com>
2023-05-13 17:14:36 +0300

    More work done on home logged in page

--- src/pages/HomeLoggedIn.js
+++ src/pages/HomeLoggedIn.js
@@ -8,15 +8,31 @@ import {
 } from "react-router-dom";
 import "../style/homeLoggedIn.css";
 import Navbar from "../componnents/NavbarLoggedIn";
-import Footer from "../componnents/Footer";
-import Sidebar from "../componnents/Sidebar";
 
 function HomeLoggedIn() {
   return (
     <div id="home-main">
       <Navbar />
-      <Sidebar />
-      <Footer />
+      <div className="kahoots my-kahoots">
+        <h1 className="kahoot-header">My kahoots</h1>
+        <hr className="break-line"></hr>
+        <ul className="kahoots-list">
+          <li className="kahoot-list-item">
+            <div className="kahoot-image">
+              <img src={require("../images/kahoot-image.webp")} />
+              <p className="kahoot-image-questions-large">X questions</p>
+              <p className="kahoot-image-questions-small">X q</p>
+            </div>
+            <div className="kahoot-info">
+              <p>Kahoot name</p>
+              <div className="kahoot-info-extra">
+                <p className="kahoot-info-extra-username">DKLearning_US</p>
+                <p className="kahoot-info-extra-plays">12k plays</p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
