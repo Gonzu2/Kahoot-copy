44b6481b1142e79ec1fec72f6b4d932021ca87be
Gonzu2 <gonzuwu2@gmail.com>
2023-05-24 17:42:03 +0300

    Yap

--- src/componnents/QuestionCard.js
+++ src/componnents/QuestionCard.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import "../style/createQuizCard.css"
-import { useEffect,useState } from "react";
-function QuestionCard({ questionCardInfo, changeQuestion, onDelete}) {
-  const [info, setInfo] = useState(questionCardInfo)
-=======
 import "../style/createQuizCard.css";
 import { useEffect, useState } from "react";
-function QuestionCard({ questionCardInfo, changeQuestion }) {
+function QuestionCard({ questionCardInfo, changeQuestion, onDelete }) {
   const [info, setInfo] = useState(questionCardInfo);
->>>>>>> ef86483e770a1b40ebd1c73f50761c6ebb2dde81
   useEffect(() => {
     if (questionCardInfo) {
       setInfo(questionCardInfo);
@@ -16,28 +9,24 @@ function QuestionCard({ questionCardInfo, changeQuestion }) {
   }, [questionCardInfo]);
   const handleClick = (e) => {
     changeQuestion(e);
-<<<<<<< HEAD
-  }
+  };
 
   const handleDelete = (e) => {
     e.preventDefault();
-    onDelete(info.index)
-
-  }
-  return (
-    <div className="question-card">
-            <h1 className="question-card-name">{info.title}</h1>
-
-            <h1 className="question-card-delete" onClick={handleDelete}>Delete</h1>
-            <div className="question-card-number" onClick={handleClick} data-key={info.index}>{info.index + 1}</div>
-=======
+    onDelete(info.index);
   };
   return (
-    <div className="question-card" onClick={handleClick} data-key={info.index}>
+    <div className="question-card">
       <h1 className="question-card-name">{info.title}</h1>
-      <h1 className="question-card-delete">Delete</h1>
-      <div className="question-card-number">{info.index + 1}</div>
->>>>>>> ef86483e770a1b40ebd1c73f50761c6ebb2dde81
+      <h1 className="question-card-delete" onClick={handleDelete}>
+        Delete
+      </h1>
+      <div
+        className="question-card-number"
+        onClick={handleClick}
+        data-key={info.index}>
+        {info.index + 1}
+      </div>
     </div>
   );
 }
